01d2fd129dbd5ada69ab96c96285a9bb4faf8c8c
kalkalkida <kalkalkida@github.com>
2026-02-28 22:20:28 +0300

    Enable SSL in PG pool and log config

--- server/db.js
+++ server/db.js
@@ -1,13 +1,18 @@
 const { Pool } = require("pg")
 require("dotenv").config()
 
-const pool = new Pool({
+const poolConfig = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_DATABASE || process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-})
+  ssl: {
+    rejectUnauthorized: false,
+  },
+}
+console.log("[v0] pool config", poolConfig)
+const pool = new Pool(poolConfig)
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
